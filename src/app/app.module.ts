c811482a8cde1953c2f88e87a8fdfb908dd7dcca
Dany Paredes <danywalls4@gmail.com>
2023-07-27 16:49:03 +0200

    refactor: :fire: remove dead files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,7 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppComponent} from './app.component';
-import {HomeComponent} from './home/home.component';
-import { NbaCalendarComponent } from './components/nba-calendar/nba-calendar.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
